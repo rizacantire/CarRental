805d7f05280c005dd56776e66325af31c790d4d4
Rıza Can TİRE <riza_tire@hotmail.com>
2021-03-07 00:42:03 +0300

    Add project files.

---
+++ WebAPI/Controllers/ColorsController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorsController : ControllerBase
+    {
+        IColorService _colorService;
+
+        public ColorsController(IColorService colorService)
+        {
+            _colorService = colorService;
+        }
+
+        [HttpGet("getall")]
+        public List<Color> GetAll()
+        {
+            var result = _colorService.GetAll();
+
+            
+
+            return result.Data;
+        }
+    }
+}
